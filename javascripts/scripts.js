29d0ed6bf5bf230e310844e8d471fb3cbb020abb
juneate <roccopan@gmail.com>
2015-06-24 19:53:46 -0400

    dump results to the page

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,10 +1,21 @@
+// The url and query string we want to search
 var url = "http://roccopanacci.com/tw/twittersearch.php?callback=?";
 var params = { 'q':'Bitmaker' };
 
+// Go make the request to Twitter
 $.getJSON(url, params, function(result){
 
-	console.log(result);
+	// For each "status" that's returned
+	$.each(result.statuses, function(num, tweet) {
+
+		// Dump the tweet text to the console
+		console.log(tweet.text);
+
+		// Dump the tweet text to the interface
+		$('<p>').text(tweet.text).appendTo('#tweets');
+
+	});
 
 });
 
-
+// DONE!

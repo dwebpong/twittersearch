11b78d62aa8f90f479efe82ef17ba5026f4f0302
juneate <roccopan@gmail.com>
2015-06-24 20:44:39 -0400

    allow user search

--- README.md
+++ README.md
@@ -10,3 +10,12 @@
     - name
     - image
   - date
+
+## Things to do
+1. Bring back a list of tweets using a generic q value
+2. Print the result to console
+3. Print just the tweet.text to the console
+4. Add the tweet.text to the page
+5. Make the button actually do something (search function)
+6. Empty the results with each search
+7. Get the search term!
--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,21 +1,31 @@
-// The url and query string we want to search
-var url = "http://roccopanacci.com/tw/twittersearch.php?callback=?";
-var params = { 'q':'Bitmaker' };
+var dothesearch = function()
+{
+	// Clear out the result set
+	$('#tweets').empty();
 
-// Go make the request to Twitter
-$.getJSON(url, params, function(result){
+	// Get the term from the input box
+	var term = $('#search').val();
 
-	// For each "status" that's returned
-	$.each(result.statuses, function(num, tweet) {
+	// The url and query string we want to search
+	var url = "http://roccopanacci.com/tw/twittersearch.php?callback=?";
+	var params = { 'q':term };
 
-		// Dump the tweet text to the console
-		console.log(tweet.text);
+	// Go make the request to Twitter
+	$.getJSON(url, params, function(result){
 
-		// Dump the tweet text to the interface
-		$('<p>').text(tweet.text).appendTo('#tweets');
+		// For each "status" that's returned
+		$.each(result.statuses, function(num, tweet) {
+
+			// Dump the tweet text to the console
+			console.log(tweet.text);
+
+			// Dump the tweet text to the interface
+			$('<p>').text(tweet.text).appendTo('#tweets');
+
+		});
 
 	});
 
-});
+};
 
-// DONE!
+$('#dotwitter').on('click', dothesearch);
